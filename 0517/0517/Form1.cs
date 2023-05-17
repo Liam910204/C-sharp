ac4b03cdc697b7af8c2e4c88d0c26d15ce7bf9c7
Liam910204 <77436651+Liam910204@users.noreply.github.com>
2023-05-17 17:46:09 +0800

    Add files via upload

---
+++ 0517/0517/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0517
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1.NME = textBox1.Text;
+            Form2 form2 = new Form2();
+            form2.IsClosed += () =>
+            {
+                form2 = null;
+                Enabled = true;
+            };
+            form2.Show();
+            Enabled = false;
+        }
+    }
+}
---
+++ 0517/0517/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0517
+{
+    public partial class Form2 : Form
+    {
+        public delegate void Del();
+        public event Del IsClosed;
+        public Form2()
+        {
+            InitializeComponent();
+            TopMost = true;
+            label1.Text = Class1.NME;
+        }
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            IsClosed?.Invoke();
+        }
+    }
+}
