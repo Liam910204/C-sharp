184adb708dca2f2d1630db7115bb87588909216d
Liam910204 <77436651+Liam910204@users.noreply.github.com>
2023-05-24 18:15:21 +0800

    Add files via upload

---
+++ 0524/0524/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace _0524
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(23, 358);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(709, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(23, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(322, 259);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.list_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(361, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(371, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "修改";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 0524/0524/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+namespace _0524
+{
+    public partial class Form1 : Form
+    {
+        private Data data = new Data();
+        private bool Onwork = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load1;
+            CreatData();
+        }
+
+        private async void Form1_Load1(object sender, EventArgs e)
+        {
+            do
+            {
+                progressBar1.Maximum = data.MaxProgress;
+                progressBar1.Value = data.Progress;
+
+                listBox1.Enabled = !Onwork;
+                textBox1.Enabled = !Onwork;
+                button1.Enabled = !Onwork;
+                await Task.Delay(16);
+            } while (true);
+        }
+
+        private async void CreatData()
+        {
+            Onwork = true;
+
+            await Task.Run(() =>
+            {               
+                data.MaxProgress = data.ItemList.GetLength(0);
+                int Len = data.MaxProgress.ToString().Length;
+                DateTime date = new DateTime();
+                string dateNew = date.ToString("yyyyMMddHHmmss");
+
+                for (int i = 0; i < data.MaxProgress; i++)
+                {
+                    string guid = Guid.NewGuid().ToString("N");
+                    data.ItemList[i, 0] = dateNew + i.ToString("D"+Len);
+                    data.ItemList[i, 1] = guid;
+                    data.Progress++;
+                }               
+            });
+            CreatItem();
+            Onwork = false;
+
+
+        }
+
+        private async void CreatItem()
+        {
+            for(int i = 0; i < data.ItemList.GetLength(0); i++)
+            {
+                listBox1.Items.Add(data.ItemList[i,0]);
+                await Task.Delay(16);
+            }
+        }
+
+        private async void list_Click(object sender, EventArgs e)
+        {
+            if (Onwork == false)
+            {
+                Onwork = true;
+                
+                if(listBox1.SelectedIndex != -1)
+                {
+                    await CreatText();
+                }
+                Onwork = false;
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (Onwork == false)
+            {
+                Onwork = true;
+
+                if (listBox1.SelectedIndex != -1)
+                {
+                    int index = listBox1.SelectedIndex;
+                    data.ItemList[index, 1] = textBox1.Text;
+
+                    await CreatText();
+                }
+                Onwork = false;
+            }
+        }
+
+        private async Task CreatText()
+        {
+            int index = listBox1.SelectedIndex;
+            int Len = data.ItemList[index, 1].Length;
+            char[] str = data.ItemList[index, 1].ToCharArray();
+            textBox1.Text = "";
+
+            data.MaxProgress = Len;
+
+            for (int i = 0; i < Len; i++)
+            {
+                textBox1.Text += str[i];
+                data.Progress++;
+                await Task.Delay(50);
+            }
+        }
+
+        private class Data
+        {
+            public string[,] ItemList { get; set; } = new string[100000, 2];
+
+            #region Progress
+            public int Progress { get; set; } = 0;
+
+            private int _MaxProgress = 0;
+            public int MaxProgress
+            {
+                get => _MaxProgress;
+                set 
+                {
+                    if(value >= 0)
+                    {
+                        _MaxProgress = value;
+                        Progress = 0;
+                    }
+                } 
+            }
+            #endregion
+        }
+
+
+    }
+}
